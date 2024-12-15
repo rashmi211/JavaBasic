92b15a52f7c07c7d44ae0ab21e49ea1edebbf362
Rashmi Salunke <salunkers213@gmail.com>
2024-12-15 20:59:31 +0530

    Restored metadata file

--- Dec2024Java/src/javaBasics/Class_Dec15.java
+++ Dec2024Java/src/javaBasics/Class_Dec15.java
@@ -1,7 +1,11 @@
 package javaBasics;
 
 public class Class_Dec15 {
+	int a=10;
 	
+	public void sum(int a) {
+		System.out.println();
+	}
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		
